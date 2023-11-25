f23239a62e6c5a49e8d383075087b1baf7ceee3a
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2023-11-25 18:13:30 +0400

    fix: add access rights in order to all users can read/update/write/delete to retail and ozon modules.

--- addons/ozon/__manifest__.py
+++ addons/ozon/__manifest__.py
@@ -1,52 +1,40 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ozon",
-
-    'summary': """
+    "name": "ozon",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['base', 'retail'],
-
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["base", "retail"],
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/menu.xml',
-        
-        'views/price/menu.xml',
-        'views/price/count_price.xml',
-        'views/price/price_history.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/menu.xml",
+        "views/price/menu.xml",
+        "views/price/count_price.xml",
+        "views/price/price_history.xml",
         # 'views/price/our_price_history.xml',
-        'views/price/our_fix_price.xml',
-        'views/price/pricing.xml',
-
-        'views/lots/lots.xml',
-
-        'views/categories/categories.xml',
-
-        'views/commissions/menu.xml',
-        'views/commissions/local_index.xml',
-        'views/commissions/fee_ozon.xml',
-        'views/commissions/logistics_price.xml',
-
-        'views/movement_of_funds/movement_of_funds.xml',
-
-        'views/import/import.xml',
-
-        'views/competitors/menu.xml',
-        'views/competitors/products_competitors.xml',
-        'views/competitors/price_competitors.xml',
+        "views/price/our_fix_price.xml",
+        "views/price/pricing.xml",
+        "views/lots/lots.xml",
+        "views/categories/categories.xml",
+        "views/commissions/menu.xml",
+        "views/commissions/local_index.xml",
+        "views/commissions/fee_ozon.xml",
+        "views/commissions/logistics_price.xml",
+        "views/movement_of_funds/movement_of_funds.xml",
+        "views/import/import.xml",
+        "views/competitors/menu.xml",
+        "views/competitors/products_competitors.xml",
+        "views/competitors/price_competitors.xml",
     ],
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
--- addons/retail/__manifest__.py
+++ addons/retail/__manifest__.py
@@ -1,32 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "retail",
-
-    'summary': """
+    "name": "retail",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "StrunkGroove",
-    'website': "#",
-    'category': 'Retail',
-    'version': '0.1',
-
-    'depends': ['base'],
-
-    'data': [
-        # 'security/ir.model.access.csv',
+    "author": "StrunkGroove",
+    "website": "#",
+    "category": "Retail",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
         # 'views/views.xml',
-        'views/menu.xml',
-        'views/cost_price/cost_price.xml',
-        'views/import/import.xml',
-        'views/products/products.xml',
-        'views/seller/seller.xml',
+        "views/menu.xml",
+        "views/cost_price/cost_price.xml",
+        "views/import/import.xml",
+        "views/products/products.xml",
+        "views/seller/seller.xml",
     ],
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
