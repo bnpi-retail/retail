e83f6d73ef028b87afaae3ab41a876ba8f2ed75d
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2023-11-30 12:11:04 +0400

    feat: Download products from Ozon API in .csv file. Add to ozon.import_file possibility to mass import products from .csv and populate ozon.products, ozon.categories, retail.products and retail.seller models.

--- addons/ozon/models/localization_index.py
+++ addons/ozon/models/localization_index.py
@@ -5,21 +5,21 @@ from odoo.exceptions import ValidationError
 
 
 class LocalizationIndex(models.Model):
-    _name = 'ozon.localization_index'
-    _description = 'Индекс локализации'
+    _name = "ozon.localization_index"
+    _description = "Индекс локализации"
 
-    lower_threshold = fields.Float(string='Нижний порог')
-    upper_threshold = fields.Float(string='Верхний порог')
-    coefficient = fields.Float(string='Коэффициент')
-    percent = fields.Float(string='Процент, %')
+    lower_threshold = fields.Float(string="Нижний порог")
+    upper_threshold = fields.Float(string="Верхний порог")
+    coefficient = fields.Float(string="Коэффициент")
+    percent = fields.Float(string="Процент, %")
 
-    @api.constrains('lower_threshold')
+    @api.constrains("lower_threshold")
     def _check_lower_threshold(self):
         for record in self:
             if record.lower_threshold < 0:
                 raise ValidationError("Нижний порог не может быть меньше 0.")
 
-    @api.constrains('upper_threshold')
+    @api.constrains("upper_threshold")
     def _upper_threshold(self):
         for record in self:
             if record.upper_threshold > 100:
@@ -27,16 +27,21 @@ class LocalizationIndex(models.Model):
 
     def name_get(self):
         """
-        Rename name records 
+        Rename name records
         """
         result = []
         for record in self:
-            result.append((record.id, f'{record.lower_threshold} '
-                           f'- {record.upper_threshold}, '
-                           f'Коэфф. = {record.coefficient}, '
-                           f'Процент = {record.percent}'))
+            result.append(
+                (
+                    record.id,
+                    f"{record.lower_threshold} "
+                    f"- {record.upper_threshold}, "
+                    f"Коэфф. = {record.coefficient}, "
+                    f"Процент = {record.percent}",
+                )
+            )
         return result
-    
+
     # @api.constrains('coefficient')
     # def _coefficient(self):
     #     for record in self:
--- addons/ozon/models/products.py
+++ addons/ozon/models/products.py
@@ -4,35 +4,40 @@ from odoo import models, fields, api
 
 
 class Product(models.Model):
-    _name = 'ozon.products'
-    _description = 'Лоты'
+    _name = "ozon.products"
+    _description = "Лоты"
 
-    categories = fields.Many2one('ozon.categories', string='Название категории')
-    id_on_platform = fields.Char(string='ID на площадке')
-    full_categories = fields.Char(string='Наименоваие раздела')
-    products = fields.Many2one('retail.products', string='Товар')
-    seller = fields.Many2one('retail.seller', string='Продавец')
-    index_localization = fields.Many2one('ozon.localization_index', 
-                                         string='Индекс локализации')
-    
-    trading_scheme = fields.Selection([('FBS', 'FBS'),
-                                       ('FBO', 'FBO')],
-                                       string='Схема торговли')
-    
-    delivery_location = fields \
-        .Selection([('PC', 'ППЗ/PC'),
-                    ('PP', 'ПВЗ/PP'),
-                    ('SC', 'СЦ/SC'),
-                    ('TSC', 'ТСЦ/TSC')],
-                    string='Пункт приема товара',
-                    help=('ППЦ - Пункт приема заказов (Pickup Center), '
-                        'ПВЗ - Пункт выдачи заказов (Pickup Point), '
-                        'СЦ - Сервисный центр (Service Center)'))
+    categories = fields.Many2one("ozon.categories", string="Название категории")
+    id_on_platform = fields.Char(string="ID на площадке")
+    full_categories = fields.Char(string="Наименоваие раздела")
+    products = fields.Many2one("retail.products", string="Товар")
+    seller = fields.Many2one("retail.seller", string="Продавец")
+    index_localization = fields.Many2one(
+        "ozon.localization_index", string="Индекс локализации"
+    )
 
+    trading_scheme = fields.Selection(
+        [
+            ("FBS", "FBS"),
+            ("FBO", "FBO"),
+            ("-", "-"),
+        ],
+        string="Схема торговли",
+    )
+
+    delivery_location = fields.Selection(
+        [("PC", "ППЗ/PC"), ("PP", "ПВЗ/PP"), ("SC", "СЦ/SC"), ("TSC", "ТСЦ/TSC")],
+        string="Пункт приема товара",
+        help=(
+            "ППЦ - Пункт приема заказов (Pickup Center), "
+            "ПВЗ - Пункт выдачи заказов (Pickup Point), "
+            "СЦ - Сервисный центр (Service Center)"
+        ),
+    )
 
     def name_get(self):
         """
-        Rename name records 
+        Rename name records
         """
         result = []
         for record in self:
