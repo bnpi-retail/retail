731f05c4ddc5782ccc3c14d15a09884348aca3b1
StrunkGroove <jmarkj@mail.ru>
2023-12-04 11:57:17 +0300

    Remove temporal from tracking and delete from filesystem

---
+++ odoo/addons/ozon/models/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from . import conditions_placing_ozon
+from . import ozon_fee
+from . import logistics_price
+from . import localization_index
+from . import products
+from . import prices
+from . import import_file
+from . import categories
+
+# from .competitors import competitors
+# from .competitors import act_competitors
+from .competitors import products_competitors
+from .competitors import price_history_competitors
+from .price import pricing
+# from .price import our_price_history
+from .movement_of_funds import movement_of_funds
+from .count_insurance_percent import count_insurance_percent
+from .search_queries import search_queries
+from .issue_report import issue_report
---
+++ odoo/addons/retail/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .import_file import import_file
+
+from . import categories
+from . import unique_product
+# from . import cost_on_platform
+from . import cost_price
+from . import seller
+from . import products
