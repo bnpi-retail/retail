28e78afb05d6a081684c16afc37f31c4c3714860
StrunkGroove <jmarkj@mail.ru>
2023-12-04 21:25:12 +0300

    add temporalio

--- addons/retail/controllers/controllers.py
+++ addons/retail/controllers/controllers.py
@@ -1,21 +1,9 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+
+from odoo import http
 
 
-# class Retail(http.Controller):
-#     @http.route('/retail/retail', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/retail/retail/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('retail.listing', {
-#             'root': '/retail/retail',
-#             'objects': http.request.env['retail.retail'].search([]),
-#         })
-
-#     @http.route('/retail/retail/objects/<model("retail.retail"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('retail.object', {
-#             'object': obj
-#         })
+class Retail(http.Controller):
+    @http.route('/retail/improt_file_1C', auth='public', csrf=False)
+    def improt_file_1C(self, **kw):
+        model_retail_import_file = http.request.env['retail.import_file']
+        return model_retail_import_file.fill_product_from_1C()
