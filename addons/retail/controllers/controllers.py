537ac35da5cbfd6cd6f249a5ef2ee4466be42046
StrunkGroove <jmarkj@mail.ru>
2023-12-06 13:54:22 +0300

    fix prod

--- addons/retail/controllers/controllers.py
+++ addons/retail/controllers/controllers.py
@@ -5,5 +5,9 @@ from odoo import http
 class Retail(http.Controller):
     @http.route('/retail/improt_file_1C', auth='public', csrf=False)
     def improt_file_1C(self, **kw):
+        
         model_retail_import_file = http.request.env['retail.import_file']
-        return model_retail_import_file.fill_product_from_1C()
+        values = {'data_for_download': 'cost_price'}
+        model_retail_import_file.create(values)
+        
+        return "Import successful"
