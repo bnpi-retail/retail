0c37681f24eac7669506fe51cb8139901ea26590
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2023-12-21 15:50:36 +0400

    feat: add populate_search_queries() to ozon.products. it takes string keywords while importing products, splits it up, and add a keyword if it doesn't already exist in that ozon.product

--- addons/ozon/helpers.py
+++ addons/ozon/helpers.py
@@ -1,3 +1,22 @@
 def split_list(l, n):
     k, m = divmod(len(l), n)
     return (l[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
+
+
+def split_keywords(keywords_string: str) -> list:
+    words = []
+    l = keywords_string.split(";")
+    for w in l:
+        if w == "" or w == " ":
+            continue
+
+        if "," in w:
+            for i in w.split(","):
+                if i == "" or i == " ":
+                    continue
+                else:
+                    words.append(i)
+        else:
+            words.append(w)
+    words = [word.strip() for word in words]
+    return words
