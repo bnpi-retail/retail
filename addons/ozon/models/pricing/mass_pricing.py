d665201288400cbc2ed5ef60ec710c343cb193d9
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2023-12-29 17:03:17 +0400

    feat: add create_mass_pricing_from_low_price_task() in ozon.tasks

--- addons/ozon/models/pricing/mass_pricing.py
+++ addons/ozon/models/pricing/mass_pricing.py
@@ -11,7 +11,30 @@ class MassPricing(models.Model):
             ("applied", "Применено"),
         ],
         string="Статус",
+        default="created",
     )
     product = fields.Many2one("ozon.products", string="Товар Ozon")
+    price = fields.Float(string="Текущая цена")
     new_price = fields.Float(string="Новая цена")
     comment = fields.Text(string="Причина")
+
+    def create_from_task(self, task_record):
+        prod = task_record.product
+        price = round(prod.price, 2)
+        profit = round(prod.profit, 2)
+        profit_delta = round(prod.profit_delta, 2)
+        profit_ideal = round(prod.profit_ideal, 2)
+        if profit < 0:
+            comment = f"Торгуем в убыток: прибыль от актуальной цены {profit}"
+        elif profit_delta < 0:
+            comment = f"Прибыль от актуальной цены {profit} меньше, чем идеальная прибыль {profit_ideal}"
+        new_price = prod.price + abs(profit_delta)
+
+        self.create(
+            {
+                "product": prod.id,
+                "price": price,
+                "new_price": new_price,
+                "comment": comment,
+            }
+        )
