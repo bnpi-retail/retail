f9dfd4204ba79f416911222992facbdeda767b09
StrunkGroove <jmarkj@mail.ru>
2024-01-17 13:06:17 +0300

    fix controllers with images

--- addons/ozon/controllers/draw_odoo.py
+++ addons/ozon/controllers/draw_odoo.py
@@ -3,6 +3,7 @@ import ast
 
 from itertools import groupby
 from operator import attrgetter
+from datetime import datetime 
 
 from odoo import http
 from odoo.http import Response
@@ -29,14 +30,29 @@ class DrawOdooController(http.Controller):
             if product not in data_for_graph:
                 data_for_graph[product.id] = {"dates": [], "qty": [], "revenue": []}
 
+            # Сортировка записей по дате
             records_list.sort(key=attrgetter('date'))
-            grouped_records = {date.strftime("%Y-%m-%d"): list(group) for date, group in groupby(records_list, key=lambda x: x.date)}
-            serialized_records = [{"date": date, "qty": record.qty} for date, group in grouped_records.items() for record in group]
 
+            # Группировка записей по дате
+            grouped_records = {date.strftime("%Y-%m-%d"): list(group) for date, group in groupby(records_list, key=lambda x: x.date)}
+
+            # Получение списка всех недель в течение года
+            all_weeks = {datetime.strptime(f'2022-W{i}-1', "%Y-W%W-%w").strftime("%Y-%m-%d"): [] for i in range(1, 53)}
+
+            # Заполнение данных для каждой недели
+            for week, records in all_weeks.items():
+                if week in grouped_records:
+                    # Если есть записи для недели, добавляем их в данные
+                    all_weeks[week] = [{"date": date, "qty": record.qty, "revenue": record.revenue} for date, group in grouped_records.items() for record in group]
+                else:
+                    # Если нет записей, добавляем нулевые значения
+                    all_weeks[week] = [{"date": week, "qty": 0, "revenue": 0}]
+
+            # Преобразование данных в список словарей для сериализации
+            serialized_records = [record for week_records in all_weeks.values() for record in week_records]
+
+            # Добавление группированных данных в data_for_graph
             data_for_graph[product.id] = serialized_records
-            # for record in records_list:
-            #     data_for_graph[product.id]["dates"].append(record.date.strftime("%Y-%m-%d"))
-            #     data_for_graph[product.id]["qty"].append(record.qty)
 
         json_response = json.dumps(data_for_graph)
         return http.Response(json_response, content_type="application/json")
