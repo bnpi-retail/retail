aa7388bc4a8ba956da07e35913fa618edf210913
StrunkGroove <jmarkj@mail.ru>
2024-01-17 10:21:50 +0300

    add controller draw

---
+++ addons/ozon/controllers/draw_odoo.py
@@ -0,0 +1,66 @@
+import json
+import ast
+
+from odoo import http
+from odoo.http import Response
+
+
+class DrawOdooController(http.Controller):
+    @http.route("/api/v1/get-all-skus-of-lots", 
+                auth="user", 
+                csrf=False,
+                methods=["GET"])
+    def get_data_for_draw(self, **kwargs):
+        model_sale = http.request.env["ozon.sale"]
+        records = model_sale.search([("is_calculate", "=", True)])
+
+        data = {}
+        for record in records:
+            if record.product not in data:
+                data[record.product] = []
+            data[record.product].append(record)
+        
+        data_for_graph = {}
+        for records in data.values():
+            if records[0].product not in data_for_graph:
+                data_for_graph[records[0].product] = []
+
+            dates = records.mapped("date")
+            qty = records.mapped("qty")
+
+            data_for_graph[records[0]]["dates"] = dates
+            data_for_graph[records[0]]["qty"] = qty
+
+        json_response = json.dumps(data_for_graph)
+        return http.Response(json_response, content_type="application/json")
+
+    @http.route("/api/v1/save-numbers-of-products-history", 
+                auth="user", 
+                type="http", 
+                csrf=False, 
+                methods=["POST"])
+    def save_images(self, data, **post):
+        data = ast.literal_eval(data)
+        
+        model_products = http.request.env["ozon.products"]
+        model_stock = http.request.env["ozon.stock"]
+
+        for sku, info in data.items():
+            product = model_products \
+                .search([("id_on_platform", "=", sku)], limit=1)
+            
+            model_stock.create({
+                "product": product.id,
+                "stocks_fbs": info["present"],
+                "stocks_reserved_fbs": info["reserved"],
+            })
+
+        response_data = {"response": "success", "message": "Processed successfully"}
+        response_json = json.dumps(response_data)
+        status_code = 200
+
+        return Response(
+            response=response_json,
+            status=status_code,
+            content_type="application/json"
+        )
