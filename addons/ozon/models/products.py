d2aa56e26d51c7b74320bb7031fcb4c5f4de1fb0
StrunkGroove <jmarkj@mail.ru>
2024-01-18 16:48:18 +0300

    fix

--- addons/ozon/models/products.py
+++ addons/ozon/models/products.py
@@ -739,8 +739,8 @@ class Product(models.Model):
 
         if response.status_code != 200:
             raise ValueError(response.status_code)
-        raise ValueError(response.status_code)
-        # raise ValueError(response.json())
+        # raise ValueError(response.status_code)
+        raise ValueError(response.json())
     
     def create_mass_pricing(self):
         self.ensure_one()
