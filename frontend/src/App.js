9ee8280420769460644e233cbddd4f3f877a4bfe
StrunkGroove <jmarkj@mail.ru>
2024-01-18 09:32:20 +0300

    del file in fornt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import AddApiToken from "./pages/AddApiToken";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
