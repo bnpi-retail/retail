70da2d4fbec0598e50808702a1061fa96d7c5381
StrunkGroove <jmarkj@mail.ru>
2024-01-19 09:58:34 +0300

    fix

--- django/app/storage_images/views.py
+++ django/app/storage_images/views.py
@@ -52,24 +52,24 @@ class DrawGraph(APIView):
         if not dates or not num:
             return [], []
 
-        # df = pd.DataFrame({'date': pd.to_datetime(dates), 'num': num})
+        df = pd.DataFrame({'date': pd.to_datetime(dates), 'num': num})
 
-        # df.set_index('date', inplace=True)
-
-        # full_date_range = pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31')
-
-        # df = df.reindex(full_date_range, fill_value=0)
-
-        # weekly_data = df.resample('W-Mon').sum()
-
-        # grouped_dates = weekly_data.index.strftime('%Y-%m-%d').tolist()
-        # grouped_num = weekly_data['num'].tolist()
-
-        df = pd.DataFrame({'date': pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31', freq='D'), 'num': 0})
         df.set_index('date', inplace=True)
+
+        full_date_range = pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31')
+
+        df = df.reindex(full_date_range, fill_value=0)
+
         weekly_data = df.resample('W-Mon').sum()
+
         grouped_dates = weekly_data.index.strftime('%Y-%m-%d').tolist()
         grouped_num = weekly_data['num'].tolist()
+
+        # df = pd.DataFrame({'date': pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31', freq='D'), 'num': 0})
+        # df.set_index('date', inplace=True)
+        # weekly_data = df.resample('W-Mon').sum()
+        # grouped_dates = weekly_data.index.strftime('%Y-%m-%d').tolist()
+        # grouped_num = weekly_data['num'].tolist()
 
         return grouped_dates, grouped_num
 
