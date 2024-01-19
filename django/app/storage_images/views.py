ce329c6e26898e2a8c1c69ba2b945dad1b930fe4
StrunkGroove <jmarkj@mail.ru>
2024-01-19 17:25:42 +0300

    final fix controllers delete dublicate stock records

--- django/app/storage_images/views.py
+++ django/app/storage_images/views.py
@@ -75,20 +75,20 @@ class DrawGraph(APIView):
         dates = data.get('dates', [])
         num = data.get('num', [])
 
-        df = pd.DataFrame({'date': pd.to_datetime(dates), 'num': num})
+        # df = pd.DataFrame({'date': pd.to_datetime(dates), 'num': num})
 
-        df = df.drop_duplicates(ignore_index=True)
+        # df = df.drop_duplicates(ignore_index=True)
 
-        df.set_index('date', inplace=True)
+        # df.set_index('date', inplace=True)
 
-        full_date_range = pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31')
+        # full_date_range = pd.date_range(start=f'{year}-01-01', end=f'{year}-12-31')
 
-        df = df.reindex(full_date_range, fill_value=0)
+        # df = df.reindex(full_date_range, fill_value=0)
 
-        weekly_data = df.resample('W-Mon').sum()
+        # weekly_data = df.resample('W-Mon').sum()
 
-        grouped_dates = weekly_data.index.strftime('%Y-%m-%d').tolist()
-        grouped_num = weekly_data['num'].tolist()
+        # grouped_dates = weekly_data.index.strftime('%Y-%m-%d').tolist()
+        # grouped_num = weekly_data['num'].tolist()
 
         return dates, num
 
