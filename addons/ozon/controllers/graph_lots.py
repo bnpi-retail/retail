61f7c6911254444924af400ac3ce206744e591be
StrunkGroove <jmarkj@mail.ru>
2024-01-19 10:07:29 +0300

    fix

--- addons/ozon/controllers/graph_lots.py
+++ addons/ozon/controllers/graph_lots.py
@@ -10,9 +10,9 @@ class OzonUrlsImagesLotsImport(http.Controller):
     )
     def import_ozon_urls_images_lots(self, **kwargs):
         model_ozon_import_file = http.request.env["ozon.import_file"]
-        # model_ozon_import_file.create({
-        #     "data_for_download": "ozon_urls_images_lots",
-        #     "file": kwargs.get("file").read().decode("utf-8"),
-        # })
+        model_ozon_import_file.create({
+            "data_for_download": "ozon_urls_images_lots",
+            "file": kwargs.get("file").read().decode("utf-8"),
+        })
 
         return "File uploaded and processed successfully."
