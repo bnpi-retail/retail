34a038888e9cfc43fcb0e915045394bf226640fe
StrunkGroove <jmarkj@mail.ru>
2024-01-19 08:51:14 +0300

    fix

--- django/app/storage_images/views.py
+++ django/app/storage_images/views.py
@@ -87,7 +87,7 @@ class DrawGraph(APIView):
         files = {'file': ('output.csv', csv_data)}
 
         # response = requests.post(endpoint, headers=headers, files=files)
-        return Response({'message': f"{product_id}--{dates}--{num}"})
+        return Response({'message': f"{product_id}--{len(dates)}--{len(num)}"})
     
         if response.status_code != 200:
             return Response({'message': 'Bad Request'}, status=400)
