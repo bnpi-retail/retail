9f05aaa160a5fa39cf36a1c649578d4b132ba32a
StrunkGroove <jmarkj@mail.ru>
2024-01-19 16:05:55 +0300

    update delete controller

--- addons/ozon/controllers/delete_dublicate_stock_records.py
+++ addons/ozon/controllers/delete_dublicate_stock_records.py
@@ -3,7 +3,7 @@ from itertools import groupby
 from odoo import http
 from odoo.http import Response
 from datetime import datetime
-
+from collections import defaultdict
 
 class AnalysysDataLotsController(http.Controller):
     @http.route("/api/v1/delete_dublicate_stock_records", 
@@ -16,13 +16,14 @@ class AnalysysDataLotsController(http.Controller):
         
         target_date = datetime.strptime('01.16.24', '%m.%d.%y')
         records = model_products.search([('timestamp', '=', target_date.strftime('%Y-%m-%d %H:%M:%S'))])
-        sorted_records = records.sorted(key=lambda r: (r.product.id, r.timestamp), reverse=True)
-        grouped_records = {key: list(group) for key, group in groupby(sorted_records, key=lambda r: r.product.id)}
+
+        latest_records = defaultdict(lambda: None)
 
         records_to_keep = model_products
-        for product_id, group in grouped_records.items():
-            latest_record = max(group, key=lambda r: r.timestamp)
-            records_to_keep |= latest_record
+        for record in records.sorted(key=lambda r: (r.product.id, r.timestamp), reverse=True):
+            if latest_records[record.product.id] is None:
+                latest_records[record.product.id] = record
+                records_to_keep |= record
 
         response_data = {"response": "success", "message": f"Records for delete: {len(records_to_keep)}"}
         response_json = json.dumps(response_data)
