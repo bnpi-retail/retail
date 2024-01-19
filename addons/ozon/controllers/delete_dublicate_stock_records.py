59bee891b56ae7db350151e1f227e52170071c40
StrunkGroove <jmarkj@mail.ru>
2024-01-19 16:33:14 +0300

    final fix controllers delete dublicate stock records

--- addons/ozon/controllers/delete_dublicate_stock_records.py
+++ addons/ozon/controllers/delete_dublicate_stock_records.py
@@ -1,9 +1,9 @@
 import json
-from itertools import groupby
+
 from odoo import http
 from odoo.http import Response
 from datetime import datetime
-from collections import defaultdict
+
 
 class AnalysysDataLotsController(http.Controller):
     @http.route("/api/v1/delete_dublicate_stock_records", 
@@ -12,22 +12,22 @@ class AnalysysDataLotsController(http.Controller):
                 csrf=False, 
                 methods=["GET"])
     def delete_dublicate_stock_records(self, **post):
-        model_products = http.request.env["ozon.stock"]
+        model_stock = http.request.env["ozon.stock"]
         
         target_date = datetime.strptime('01.16.24', '%m.%d.%y')
-        records = model_products.search([('timestamp', '=', target_date.strftime('%Y-%m-%d %H:%M:%S'))])
+        records = model_stock.search([('timestamp', '=', target_date.strftime('%Y-%m-%d %H:%M:%S'))])
 
         records_unique = set()
         records_to_remove = []
 
         for record in records:
             if record.product.id in records_unique:
-                records_to_remove.append(record)
+                records_to_remove.append(record.id)
             else:
                 records_unique.add(record.product.id)
 
-        for record in records_to_remove:
-            record.unlink()
+        for id in records_to_remove:
+            model_stock.search([("id", "=", id)]).unlink()
 
         response_data = {"response": "success", "message": f"Records for delete: {len(records_to_remove)}"}
         response_json = json.dumps(response_data)
