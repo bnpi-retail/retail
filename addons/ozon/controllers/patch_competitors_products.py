bc68557a15a06df4452b730ccffb39b6febc1ace
StrunkGroove <jmarkj@mail.ru>
2024-01-22 08:31:55 +0300

    fix

--- addons/ozon/controllers/patch_competitors_products.py
+++ addons/ozon/controllers/patch_competitors_products.py
@@ -25,11 +25,9 @@ class PatchCompetitorsProductsController(http.Controller):
             product = model_products.search([("id", "=", product_id)])
 
             new = True
-            for price_history_id in product.competitors_with_price_ids:
-                price_history_record = model_price_history_competitors.search([("id", "=", price_history_id)])
-                
-                if price_history_competitor_record.product_competitors.id == price_history_record.product_competitors.id:
-                    product.write({'competitors_with_price_ids': [(3, price_history_record.id)]})
+            for price_history in product.competitors_with_price_ids:
+                if price_history_competitor_record.product_competitors.id == price_history.product_competitors.id:
+                    product.write({'competitors_with_price_ids': [(3, price_history)]})
                     product.write({'competitors_with_price_ids': [(4, price_history_competitor_record.id)]})
                     count_patch += 1
                     new = False
