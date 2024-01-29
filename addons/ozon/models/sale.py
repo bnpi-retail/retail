0e4288a24e1f64038bc1d32ef3d9ced94911b71b
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2024-01-29 14:26:58 +0400

    feat: store product_id_on_platform in ozon.sale

--- addons/ozon/models/sale.py
+++ addons/ozon/models/sale.py
@@ -6,12 +6,16 @@ class ProductSale(models.Model):
     _description = "Продажи товаров"
 
     product = fields.Many2one("ozon.products", string="Товар Ozon")
+    product_id_on_platform = fields.Char(
+        string="Product ID",
+        readonly=True,
+        compute="_compute_product_id_on_platform",
+        store=True,
+    )
     date = fields.Date(string="Дата продажи", readonly=True)
     qty = fields.Integer(string="Кол-во проданного товара", readonly=True)
     revenue = fields.Float(string="Выручка", readonly=True)
-    is_calculate = fields.Boolean(
-        string="Учавствует в расчетах", default=False
-    )
+    is_calculate = fields.Boolean(string="Учавствует в расчетах", default=False)
     total_qty = fields.Integer(
         string="Кол-во проданного товара за всё время", computed="_compute_total_qty"
     )
@@ -30,3 +34,7 @@ class ProductSale(models.Model):
             values["total_revenue"] += rec.revenue
 
         return super(ProductSale, self).create(values)
+
+    def _compute_product_id_on_platform(self):
+        for rec in self:
+            rec.product_id_on_platform = rec.product.id_on_platform
