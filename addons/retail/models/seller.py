56e753690fe78ce80ae84a86ded3a243e8ed430a
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2024-01-30 11:35:35 +0400

    feat: add selection field tax to retail.seller

--- addons/retail/models/seller.py
+++ addons/retail/models/seller.py
@@ -4,29 +4,34 @@ from odoo import models, fields, api, exceptions
 
 
 class Seller(models.Model):
-    _name = 'retail.seller'
-    _description = 'Продавцы'
+    _name = "retail.seller"
+    _description = "Продавец"
 
-    name = fields.Char(string='Наименование продовца')
-    ogrn = fields.Char(string='ОГРН', unique=True)
-    fee = fields.Float(string='Налогообложение, %')
+    name = fields.Char(string="Название")
+    ogrn = fields.Char(string="ОГРН")
 
+    tax = fields.Selection(
+        [
+            ("earnings_minus_expenses_15", "Доходы минус расходы - 15%"),
+            ("earnings_6", "Доходы 6%"),
+        ],
+        string="Налогообложение",
+    )
 
     def name_get(self):
         """
-        Rename name records 
+        Rename name records
         """
         result = []
         for record in self:
             result.append((record.id, record.name))
         return result
-    
-    
+
     @api.model
     def create(self, values):
         # if 'ogrn' in values:
         #     ogrn = values['ogrn']
         #     if not ogrn.isdigit():
-                # raise exceptions.ValidationError('ОГРН должен быть 13-значным числом')
-        
+        # raise exceptions.ValidationError('ОГРН должен быть 13-значным числом')
+
         return super(Seller, self).create(values)
