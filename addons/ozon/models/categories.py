9549cf3a194aeb7979aabaf445abb92c46caca47
StrunkGroove <jmarkj@mail.ru>
2024-02-01 18:21:50 +0300

    fix

--- addons/ozon/models/categories.py
+++ addons/ozon/models/categories.py
@@ -21,6 +21,7 @@ class GenerateUrlForDownloadGrpahData(models.Model):
 
     def get_url(self, model_name, record_id, field_name):
         base_url = getenv("ODOO_DOMAIN")
+        raise ValueError(base_url)
         return f'{base_url}/web/content_text?model={model_name}&id={record_id}&field={field_name}'
 
     def get_download_url(self, field_name):
