107ca4739570e748d54e0ebac8e1292b5e9f42bb
StrunkGroove <jmarkj@mail.ru>
2024-02-01 11:33:41 +0300

    fix

--- addons/ozon/__manifest__.py
+++ addons/ozon/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "ozon",
+    "name": "Ozon",
     "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
