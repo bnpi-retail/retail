db79a04ee37da98c9d1723fb5d1c9635743f5135
StrunkGroove <jmarkj@mail.ru>
2024-02-01 22:49:05 +0300

    add empty file

--- addons/ozon/controllers/download_data_graph.py
+++ addons/ozon/controllers/download_data_graph.py
@@ -9,9 +9,8 @@ class DownloadDataGraph(http.Controller):
         try:
             record = request.env[model].browse(int(id))
             if record and field in record:
-                raise ValueError(912312313)
                 return request.make_response(
-                    record[field],
+                    record[field].encode('utf-8'),
                     [('Content-Type', 'text/plain'), ('Content-Disposition', f'attachment; filename={model}_{id}_{field}.txt')]
                 )
         except Exception as e:
