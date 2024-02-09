e3b989e3e31a9371dbaa052f72f9b3e42fdad65f
StrunkGroove <jmarkj@mail.ru>
2024-02-09 19:19:54 +0300

    open url in new window

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -125,7 +125,10 @@ const Contact = () => {
       {data.map((item) => (
         <li key={item.number} style={{ flexBasis: '23%', marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '20px' }}>
           {/* Переход по ссылке при клике на изображение */}
-          <div style={{ textAlign: 'center', cursor: 'pointer' }} onClick={() => { window.open(item.href, '_self'); window.open(item.href, '_blank'); }}>
+          <div style={{ textAlign: 'center', cursor: 'pointer' }} onClick={() => { 
+            window.open(item.href, '_blank'); 
+            window.location.href = item.href;
+          }}>
             <Image src={item.pictures} style={{ maxWidth: 250, height: 250, margin: '0 auto' }} />
           </div>
           {/* Выбор чекбокса */}
