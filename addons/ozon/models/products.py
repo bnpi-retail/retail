2d407b88afeef344fcc3c7848e87a4074d06c364
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2024-02-22 14:18:40 +0400

    feat: fix collect_product_data() in price_comparison

--- addons/ozon/models/products.py
+++ addons/ozon/models/products.py
@@ -2250,7 +2250,41 @@ class GenerateUrlForDownloadGrpahData(models.Model):
         url = self.get_url(model_name, record_id, field_name)
         return url
 
+class ProductExpenses(models.Model):
+    _inherit = "ozon.products"
 
+    def get_expense_by_category(self, category):
+        return self.all_expenses_ids.filtered(lambda r: r.category == category)
+    
+    @property
+    def _logistics(self):
+        return self.get_expense_by_category("Логистика")
+    
+    @property
+    def _acquiring(self):
+        return self.get_expense_by_category("Эквайринг")
+    
+    @property
+    def _last_mile(self):
+        return self.get_expense_by_category("Последняя миля")
+    
+    @property
+    def _ozon_reward(self):
+        return self.get_expense_by_category("Вознаграждение Ozon")
+    
+    @property
+    def _processing(self):
+        return self.get_expense_by_category("Обработка")
+    
+    @property
+    def _promo(self):
+        return self.get_expense_by_category("Продвижение товара")
+    
+    @property
+    def _tax(self):
+        return self.get_expense_by_category("Налог")
+    
+    
 class ProductCalculator(models.Model):
     _name = "ozon.product_calculator"
     _description = "Калькулятор лота"
