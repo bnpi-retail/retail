95aaa7f77aaa472475a88cb3ba305809e6bdcc23
mechanovkirill <mechanovkirill@gmail.com>
2024-03-06 19:36:32 +0400

    Update bcg matrix computing: more checks.

--- addons/ozon/models/competitors/competitor_seller.py
+++ addons/ozon/models/competitors/competitor_seller.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class CompetitorSeller(models.Model):
@@ -6,7 +7,7 @@ class CompetitorSeller(models.Model):
     _description = "Магазин-конкурент"
 
     trade_name = fields.Char(string='Торговое название')
-    is_my_shop = fields.Char(default=False)
+    is_my_shop = fields.Boolean(readonly=True)
 
     def name_get(self):
         """
@@ -16,3 +17,17 @@ class CompetitorSeller(models.Model):
         for record in self:
             result.append((record.id, record.trade_name))
         return result
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        competitor_trade_name = vals_list.get('trade_name')
+        if competitor_trade_name and isinstance(competitor_trade_name, str):
+            competitor_trade_name = competitor_trade_name.lower()
+            our_sellers = self.env["retail.seller"].search([])
+            for seller in our_sellers:
+                trade_name: str = seller.trade_name
+                trade_name = trade_name.lower()
+                if trade_name == competitor_trade_name:
+                    vals_list['trade_name'] = trade_name + '_'
+
+        return super().create(vals_list)
