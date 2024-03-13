83007d8da443429acfa332588de6b1d8dffbba41
Aleksandr-Mamonov <116185701+Aleksandr-Mamonov@users.noreply.github.com>
2024-03-13 16:07:55 +0400

    feat: add special tree view for draft_products inside mass_calculator

--- addons/ozon/models/mass_calculator.py
+++ addons/ozon/models/mass_calculator.py
@@ -7,3 +7,15 @@ class MassCalculator(models.Model):
     category_id = fields.Many2one("ozon.categories", string="Категория товаров")
     draft_product_ids = fields.One2many("ozon.draft_product", "mass_calculator_id", 
                                         string="Товары-черновики")
+    
+    def open_mass_calculator(self):
+        view = self.env["ir.ui.view"].search([]).filtered(
+            lambda r: r.xml_id == "ozon.ozon_draft_product_in_mass_calculator_tree")
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Калькулятор",
+            "view_mode": "tree,form",
+            "res_model": "ozon.draft_product",
+            "domain": [("mass_calculator_id", "=", self.id)],
+            "views": [[view.id, "tree"], [False, "form"]],
+        }
